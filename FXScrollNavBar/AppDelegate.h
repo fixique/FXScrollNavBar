fd522a14e8b3bba0496cdf3f4bb425836f320349
Vlad Krupenko <dev.fixique@gmail.com>
2018-02-08 22:58:55 +0300

    Initial Commit

---
+++ FXScrollNavBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FXScrollNavBar
+//
+//  Created by Vlad Krupenko on 08.02.2018.
+//  Copyright © 2018 Vladislav Krupenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FXScrollNavBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FXScrollNavBar
+//
+//  Created by Vlad Krupenko on 08.02.2018.
+//  Copyright © 2018 Vladislav Krupenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
